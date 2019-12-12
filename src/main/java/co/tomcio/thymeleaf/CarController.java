749fa34b82bd999d0548a333e7d23b42b0e4cfa7
Tomasz Nosal <tnosal@kamsoft.pl>
2019-12-12 12:43:24 +0100

    list iterator

--- src/main/java/co/tomcio/thymeleaf/CarController.java
+++ src/main/java/co/tomcio/thymeleaf/CarController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class CarController {
@@ -12,9 +15,21 @@ public class CarController {
     @GetMapping("/car")
     public String getCar(Model model) {
 
-        Car car = new Car("BMW", "i8");
+        Car car1 = new Car("BMW", "i8");
+        Car car2 = new Car("Fiat", "126p");
+        Car car3 = new Car("Polonez", "Caro");
+        Car car4 = new Car("Hyundai", "i30");
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        carList.add(car4);
+
+
+
         model.addAttribute("name", "Tomcio");
-        model.addAttribute("car", car);
+        model.addAttribute("cars", carList);
 
         return "car";
     }
